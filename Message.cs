50853bc0c815d3cf137b5ec23e613e8352a95d8b
opilane <opilane@HP-CZC2349HTD>
2023-09-01 10:54:09 +0300

    Add project files.

---
+++ Message.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace like_popularity
+{
+    public class Message
+    {
+        private readonly string _content;
+        private readonly string _author;
+        private readonly DateTime _time;
+        private int _likes;
+
+        public Message(string content, string author, DateTime time)
+        {
+            this._content = content;
+            this._author = author;
+            this._time = time;
+        }
+
+        public int Likes { get => _likes; }
+        public DateTime Time { get => _time; }
+        public string Author { get => _author; }
+        public string Content { get => _content; }
+
+        public void AddLike()
+        {
+            _likes++;
+        }
+
+        public double GetPopularity()
+        {
+            double elapsed = DateTime.Now.Subtract(this._time).TotalSeconds;
+            if (elapsed == 0)
+            {
+                return _likes;
+            }
+            return _likes / elapsed;
+        }
+
+        public void ShowInfo(string Content, DateTime Time, double GetPopularity, List<double> messages)
+        {
+            Console.WriteLine(Content);
+            Console.WriteLine(Time);
+            Console.WriteLine(GetPopularity);
+            messages.Add(GetPopularity);
+        }
+    }
+}
---
+++ main.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime;
+using System.Runtime.Intrinsics.X86;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace like_popularity
+{
+    public class mian
+    {
+        public static void Main()
+        {
+            List<double> messages = new List<double>();
+
+            Message m1 = new Message("Hello", "John", DateTime.Now.AddDays(-10));
+            m1.AddLike();
+            m1.ShowInfo(m1.Content, m1.Time, m1.GetPopularity(), messages);
+            
+            Message m2 = new Message("Hi", "Mary", DateTime.Now.AddMinutes(-1));
+            m2.AddLike();
+            m2.ShowInfo(m2.Content, m2.Time, m2.GetPopularity(), messages);
+
+            int i = 0;
+            int du = 0;
+            int da = 1;
+            bool pop;
+            while (i < messages.Count) 
+            {
+                pop = messages[du] > messages[da];
+                i++;
+                du++;
+                da++;   
+            }
+
+            string popular = "";
+            if (pop == true)
+            {
+                popular = "YOU ARE POPULAR";
+            }
+            else
+            { popular = "you are not popular at all((("; }
+
+
+            //bool pop = m1.GetPopularity() < m2.GetPopularity();
+            //string popular = "";
+            //if (pop == true)
+            //{
+            //    popular = "YOU ARE POPULAR";
+            //}
+            //else
+            //{ popular = "you are not popular at all((("; }
+            //Console.WriteLine(popular);
+        }
+    }
+}
